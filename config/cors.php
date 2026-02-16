1cd495718a9d2cbfc0340c6dc8e528fc0933f6f9
Ali-Al-Noufal <alialnoufal3@gmail.com>
2026-02-16 06:11:48 -0500

    finish

---
+++ config/cors.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+'allowed_methods' => ['*'],
+
+// أضف رابط الـ React الخاص بك هنا بدقة
+'allowed_origins' => [
+    'https://alaamohammad-yf2udjy3d-alis-projects-a74f2b72.vercel.app',
+],
+
+'allowed_origins_patterns' => [],
+
+'allowed_headers' => ['*'],
+
+'exposed_headers' => [],
+
+'max_age' => 0,
+
+'supports_credentials' => true, // اجعلها true إذا كنت تستخدم ملفات تعريف الارتباط أو Sanctum
+
+];
