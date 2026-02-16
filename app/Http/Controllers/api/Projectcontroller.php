b7a70300bd3c50f119306bbf2dbce959b2a8398c
Ali-Al-Noufal <alialnoufal3@gmail.com>
2026-02-16 08:17:06 -0500

    done

--- app/Http/Controllers/api/Projectcontroller.php
+++ app/Http/Controllers/api/Projectcontroller.php
@@ -90,14 +90,23 @@ class Projectcontroller extends Controller
           'description'=>'string|required',
         ]);
             if($request->hasFile('image')){
-            $image_path=public_path('files/images'.$project->image);
-            unlink($image_path);
-            $image=$request->file('image');
-            $imagename=time().".".$image->getclientoriginalname();
-            $image->move(public_path("files/images"),$imagename);
-            $project->image=$imagename;
+             $image = $request->file('image');
+        $filename = time() . '-' . $image->getClientOriginalName();
+        
+        // قراءة محتوى الصورة
+        $fileContent = file_get_contents($image->getRealPath());
+
+        // إرسال الصورة إلى Vercel Blob API
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer ' . env('BLOB_READ_WRITE_TOKEN'),
+            'x-add-random-suffix' => 'true', // لإضافة رمز عشوائي يمنع تكرار الأسماء
+        ])->withBody($fileContent, $image->getMimeType())
+          ->put("https://blob.vercel-storage.com/" . $filename);
+            $data = $response->json();
+            $imageUrl = $data['url']; // هذا هو الرابط الدائم للصورة
         }
         $project->url=$request->url;
+        $project->image=$imageUrl;
         $project->type=$request->type;
         $project->features=$request->features;
         $project->title=strip_tags($request->title);
@@ -115,8 +124,6 @@ class Projectcontroller extends Controller
         if(empty($project)){
             return response()->json(['message'=>'project not found']);
         }
-        $image_path=public_path('files/'.$project->image);
-        unlink($image_path);
         $project->delete();
         return response()->json(['message'=>'success']);
     }
